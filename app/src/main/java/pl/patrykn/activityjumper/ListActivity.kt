eee7d10931fd0969cd83606a95c6c468bfc48135
Patryk Nowakowski <inz.p.nowakowski@gmail.com>
2017-11-29 23:48:07 +0100

    Add basic database usage using Anko library

--- app/src/main/java/pl/patrykn/activityjumper/ListActivity.kt
+++ app/src/main/java/pl/patrykn/activityjumper/ListActivity.kt
@@ -1,7 +1,7 @@
 package pl.patrykn.activityjumper
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class ListActivity : AppCompatActivity() {
 
