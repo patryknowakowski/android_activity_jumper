06c1260dd29a8b400192a6f49c6cdf213ac96cca
Patryk Nowakowski <inz.p.nowakowski@gmail.com>
2017-12-10 21:55:23 +0100

    Add AppActivity

---
+++ app/src/main/java/pl/patrykn/activityjumper/AppActivity.kt
@@ -0,0 +1,37 @@
+package pl.patrykn.activityjumper
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_app.*
+import pl.patrykn.activityjumper.components.AppItem
+import pl.patrykn.activityjumper.library.IntentCompanion
+import pl.patrykn.activityjumper.library.IntentExtra
+
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        intentCompanion.params(this) {
+            setTitle(it.app?.label)
+        }
+    }
+
+    object intentCompanion: IntentCompanion<intentExtra>(intentExtra, AppActivity::class)
+
+    object intentExtra : IntentExtra() {
+        var Intent.app by ExtraParcelable<AppItem>()
+    }
+}
